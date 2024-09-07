25a7f76539689d8b52c07c91e397da8f0ff1b795
Suraj Kannujiya <surajkannujiya517@gmail.com>
2024-09-07 13:43:48 +0530

    Set Mock Tests

--- fe/src/app/app-routing.module.ts
+++ fe/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MockTestComponent } from './MockTest/mock-test/mock-test.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Default route
+  { path: 'mock-test', component: MockTestComponent }, 
+];
+debugger
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
